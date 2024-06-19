0b3448a05caf323ff7a174d91e27e8b7ed727284
darpan <garg.darpan199628@gmail.com>
2024-06-19 23:58:56 +0530

    My 1st commit

---
+++ Session1.py
@@ -0,0 +1,11 @@
+x = int(input("Please enter the value of x: "))
+y = int(input("Please enter the value of y: "))
+
+if x<y:
+    print("x is less than y")
+    
+if x>y:
+    print("x is greater than y")
+    
+if x==y:
+    print("x is equals to y")
